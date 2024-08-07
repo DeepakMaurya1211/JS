a8064a63c42eb3f14273395519cfb2eb5adf16a9
DeepakMaurya1211 <deepakmaurya55936@gmail.com>
2024-08-07 16:50:28 +0530

    done

---
+++ Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountId = 55936
+let accountEmail = "Chicha7404@gmail.com"
+var accountCity = "Jaipur"
+
+console.table([accountId,accountEmail,accountCity]);
+// const keyword cannot be changed throughout the program
+
+ 
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Vulcan" // string type
+let age = 18 // Integer type
+let loggedIn = true  //boolean type
+
+
+// null => standalone value (representation of an empty value)
+// undefined => variable declared but no defined value
+//symbol => unique
+
+// object
+console.log(typeof undefined);
